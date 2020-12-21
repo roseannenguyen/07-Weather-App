b279e886f648ba9f43c9a465ad2693efcf7f489b
Roseanne Nguyen <rtnguye1@gmail.com>
2020-12-20 21:20:43 -0700

    update on weather assignment; working uv index!

--- app.js
+++ app.js
@@ -11,58 +11,75 @@ function getCurrentWeatherInfo() {
     }).then(function (response) {
         console.log(response)
 
-       displayCurrentWeather(response);
-  
-       storedLastWeather(response);
+        displayCurrentWeather(response);
+        getUVInfo(response)
+        storedLastWeather(response);
 
     });
 }
 
-$("#add-weather").on("click", function(event){
+$("#add-weather").on("click", function (event) {
     event.preventDefault();
     // getForecast();
     getCurrentWeatherInfo();
 
-    })
+})
 
 
 function displayCurrentWeather(weather) {
 
-$("#city").text(weather.name)
-// check string
-$("#currentDate").text(new Date(weather.dt * 1000).toLocaleDateString())
+    $("#city").text(weather.name)
+    // check string
+    $("#currentDate").text(new Date(weather.dt * 1000).toLocaleDateString())
 
-var iconcode = weather.weather[0].icon;
-var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-$("#weatherIcon").attr("src", iconurl)
-
-// math
-$("#temperature").text(((weather.main.temp - 273.15) * 1.80 + 32).toFixed(0));
-
-$("#humidity").text(weather.main.humidity)
-$("#wind").text((weather.wind.speed*2.237).toFixed(0))
+    var iconcode = weather.weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    $("#weatherIcon").attr("src", iconurl)
+    $("#temperature").text(((weather.main.temp - 273.15) * 1.80 + 32).toFixed(0));
+    $("#humidity").text(weather.main.humidity)
+    $("#wind").text((weather.wind.speed * 2.237).toFixed(0))
 
 
 }
-    
-// function getForecast() {
-  
-    
-//         for (var i = 0; i < 6; i++) {
 
-//         var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
-//         var iconDay= response.list[((i+1)*8)-1].weather[0].icon;
-//         var iconIMG="https://openweathermap.org/img/wn/"+iconDay+".png";
-//         var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
-//         var humidity= response.list[((i+1)*8)-1].main.humidity;
+function getUVInfo(weather) {
 
-//         $("#fDate"+i).text(date);
-//             $("#fImg"+i).text("<img src="+iconIMG+">");
-//             $("#fTemp"+i).text(tempF+"&#8457");
-//             $("#fHumidity"+i).text(humidity+"%");
-//         }
+    var latitude = weather.coord.lat;
+    var longitude = weather.coord.lon;
+
+    var UVqueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=111bd09519012867035e16b4e2d0ebd1"
+
+    $.ajax({
+        url: UVqueryURL,
+        method: "GET"
+    }).done(function (response) {
+        console.log(response)
+
+        $("#UV").text(response.value)
+
+
+    });
+}
+
+
+
+function getForecast() {
+
+    var UVqueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=111bd09519012867035e16b4e2d0ebd1"
+
+    $.ajax({
+        url: UVqueryURL,
+        method: "GET"
+    }).done(function (response) {
+        console.log(response)
+
+        $("#UV").text(response.value)
+
+
+    });
       
-// }
+
+}
 
 
 
@@ -72,24 +89,7 @@ $("#wind").text((weather.wind.speed*2.237).toFixed(0))
 
 
 // empty method 
-// function displayUVInfo() {
 
-//     var latitude = weather.coord.lat;
-//     var longitude = weather.coord.lon;
-
-//     var UVqueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=111bd09519012867035e16b4e2d0ebd1"
-
-//     $.ajax({
-//         url: UVqueryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(response)
-
-//         $(document).on("click", ".city", function(event){
-//         event.preventDefault();
-//         })
-//     });
-// }
 
 // function displayForecast() {
 
@@ -108,12 +108,12 @@ function storedLastWeather(city) {
 }
 
 function displayDefaultWeather() {
-   
-   var lastCity = localStorage.getItem("lastCity")
-   console.log(lastCity)
-   if (lastCity === null) {
-       getCurrentWeatherInfo(lastCity);
-   }
+
+    var lastCity = localStorage.getItem("lastCity")
+    console.log(lastCity)
+    if (lastCity === null) {
+        getCurrentWeatherInfo(lastCity);
+    }
 }
 
 displayDefaultWeather();
